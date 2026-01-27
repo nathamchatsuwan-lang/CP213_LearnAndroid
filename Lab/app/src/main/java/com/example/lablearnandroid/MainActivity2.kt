d3bedf128b686c20e5c696cf970925182bef6b23
nathamchatsuwan-lang <natham.chatsuwan@g.swu.ac.th>
2026-01-27 16:24:45 +0700

    c

--- Lab/app/src/main/java/com/example/lablearnandroid/MainActivity2.kt
+++ Lab/app/src/main/java/com/example/lablearnandroid/MainActivity2.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lablearnandroid.ui.theme.LabLearnAndroidTheme
+
+import androidx.compose.ui.unit.dp
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,11 +40,23 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    var inputText by remember { mutableStateOf("") }
+
+        Column {
+            Text(
+                text = "Hello $name!say = "+inputText,
+                modifier = modifier
+            )
+
+            TextField(
+                value = inputText,
+                onValueChange = {
+                    inputText = it
+                }
+
+            )
+        }
+    }
 
 @Preview(showBackground = true)
 @Composable
