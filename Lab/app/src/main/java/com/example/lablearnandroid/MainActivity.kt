1485dff6b561a5e6552efe36823463251dedce7b
nathamchatsuwan-lang <natham.chatsuwan@g.swu.ac.th>
2026-02-03 15:24:06 +0700

    com

--- Lab/app/src/main/java/com/example/lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/lablearnandroid/MainActivity.kt
@@ -4,22 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lablearnandroid.ui.theme.LabLearnAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,10 +58,65 @@ class MainActivity : ComponentActivity() {
                     )
                 }
                 //image
-                    
-                //status
+                Image(
+                    painter = painterResource(id = R.drawable.ic_profile),
+                    contentDescription = "Profile",
+                    modifier = Modifier.size(480.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp)
+                )
+
+                var str by remember { mutableStateOf(8) }
+                var agi by remember { mutableStateOf(10) }
+                var int by remember { mutableStateOf(15) }
+                Row (modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+
+                    Column {
+                        Button(onClick =  {
+                            str = str + 1
+                        }){
+                            Text(text = "+ ", fontSize = 32.sp)
+                        }
+                        Text(text = "Str: ", fontSize = 32.sp)
+                        Text(text = str.toString(),fontSize = 32.sp)
+
+                        Text(text = "-", fontSize = 32.sp,
+                            modifier = Modifier.clickable {  })
+
+                    }
+                    Column {
+                        Text(text = "Agi: ", fontSize = 32.sp)
+                        Text(text = agi.toString(),fontSize = 32.sp)
+                    }
+                    Column {
+                        Text(text = "Int:", fontSize = 32.sp)
+                        Text(text = "C", fontSize = 32.sp)
+                    }
+
+
+                }
+
+
             }
-            }
+
         }
     }
 
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        LabLearnAndroidTheme {
+            Greeting("Android")
+        }
+    }
+}
+                //status
