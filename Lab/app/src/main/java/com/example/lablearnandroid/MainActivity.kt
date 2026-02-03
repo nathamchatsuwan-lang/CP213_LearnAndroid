6400ceb7919a80a830122af5d8d242e1078ec87b
nathamchatsuwan-lang <natham.chatsuwan@g.swu.ac.th>
2026-02-03 14:46:51 +0700

    commit

--- Lab/app/src/main/java/com/example/lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/lablearnandroid/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     )
                 }
                 //image
-
+                    
                 //status
             }
             }
