354f4e81439f3e9d7550381cd72b3bb1a0bcf214
Felix <natham.chatsuwan@g.swu.ac.th>
2026-02-10 15:24:54 +0700

    change

--- Lab/app/src/main/java/com/example/lablearnandroid/RPGCardActivity.kt
+++ Lab/app/src/main/java/com/example/lablearnandroid/RPGCardActivity.kt
@@ -1,164 +1,147 @@
 package com.example.lablearnandroid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import android.util.Log
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 
-class PokedexActivity : ComponentActivity() {
+import android.util.Log
+
+class RPGCardActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("Lifecycle", "PokedexActivity : onCreate")
-        enableEdgeToEdge()
+        Log.i("Lifecycle", "MainActivity : onCreate")
         setContent {
-            ListScreen()
+            RPGCardView()
         }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i("Lifecycle", "PokedexActivity : onStart")
+        Log.i("Lifecycle", "MainActivity : onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i("Lifecycle", "PokedexActivity : onResume")
+        Log.i("Lifecycle", "MainActivity : onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i("Lifecycle", "PokedexActivity : onPause")
+        Log.i("Lifecycle", "MainActivity : onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("Lifecycle", "PokedexActivity : onStop")
+        Log.i("Lifecycle", "MainActivity : onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("Lifecycle", "PokedexActivity : onDestroy")
+        Log.i("Lifecycle", "MainActivity : onDestroy")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i("Lifecycle", "PokedexActivity : onRestart")
+        Log.i("Lifecycle", "MainActivity : onRestart")
     }
-}
 
-@Composable
-fun ListScreen() {
-    // ท่าง่าย
-    Column(modifier = Modifier.fillMaxSize().background(Color.Red).padding(16.dp)) {
-        Column(modifier = Modifier.fillMaxSize().background(Color.Gray).padding(16.dp)) {
-            LazyColumn(modifier = Modifier.fillMaxSize().background(Color.White).padding(16.dp)) {
-                items(allKantoPokemon) { item ->
-                    Row(
-                        modifier = Modifier.padding(8.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center) {
-                        Text(text= item.number.toString())
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Text(text= item.name)
-
-                        val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.number}.png"
-
-                        AsyncImage(
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(imageUrl)
-                                .listener(
-                                    onStart = {
-                                        Log.d("AsyncImage", "Start loading: $imageUrl")
-                                    },
-                                    onError = { _, result ->
-                                        Log.e("AsyncImage", "Error loading: $imageUrl", result.throwable)
-                                    },
-                                    onSuccess = { _, _ ->
-                                        Log.d("AsyncImage", "Success loading: $imageUrl")
-                                    }
-                                )
-                                .build(),
-                            contentDescription = "Sprite of ${item.name}",
-                            modifier = Modifier.size(64.dp),
-                            placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
-                            error = painterResource(id = R.drawable.ic_launcher_background)
-                        )
+    @Composable
+    fun RPGCardView() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Gray)
+                .padding(32.dp)) {
+            // hp
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+                .background(color = Color.White)
+            ) {
+                Text(
+                    text = "hp",
+                    modifier = Modifier
+                        .align(alignment = Alignment.CenterStart)
+                        .fillMaxWidth(fraction = 0.55f)
+                        .background(color = Color.Red)
+                        .padding(8.dp)
+                )
+            }
+            // image
+            Image(
+                painter = painterResource(R.drawable.ic_profile),
+                contentDescription = "My Image",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp, bottom = 32.dp)
+                    .clickable {
+                        startActivity(Intent(this@RPGCardActivity, LifeCycleComposeActivity::class.java))
                     }
+            )
+
+            var str by remember { mutableStateOf(8) }
+            var agi by remember { mutableStateOf(10) }
+            var int by remember { mutableStateOf(15) }
+            // status
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Button(onClick = {
+                        str = str + 1
+                    }) {
+                        Text(text = "+", fontSize = 32.sp)
+                    }
+                    Text(text = "Str", fontSize = 32.sp)
+                    Text(text = str.toString(), fontSize = 32.sp)
+                    Text(text = "-", fontSize = 32.sp, modifier = Modifier.clickable {
+                        str = str - 1
+                    })
+                }
+                Column {
+                    Text(text = "Agi", fontSize = 32.sp)
+                    Text(text = agi.toString(), fontSize = 32.sp)
+                }
+                Column {
+                    Text(text = "Int", fontSize = 32.sp)
+                    Text(text = int.toString(), fontSize = 32.sp)
                 }
             }
         }
     }
+
+    @Preview
+    @Composable
+    fun previewScreen() {
+        RPGCardView()
+    }
 }
 
-data class Pokemon(
-    val name: String,
-    val number: Int
-)
-
-val allKantoPokemon = listOf(
-    Pokemon("Bulbasaur", 1),
-    Pokemon("Ivysaur", 2),
-    Pokemon("Venusaur", 3),
-    Pokemon("Charmander", 4),
-    Pokemon("Charmeleon", 5),
-    Pokemon("Charizard", 6),
-    Pokemon("Squirtle", 7),
-    Pokemon("Wartortle", 8),
-    Pokemon("Blastoise", 9),
-    Pokemon("Caterpie", 10),
-    Pokemon("Metapod", 11),
-    Pokemon("Butterfree", 12),
-    Pokemon("Weedle", 13),
-    Pokemon("Kakuna", 14),
-    Pokemon("Beedrill", 15),
-    Pokemon("Pidgey", 16),
-    Pokemon("Pidgeotto", 17),
-    Pokemon("Pidgeot", 18),
-    Pokemon("Rattata", 19),
-    Pokemon("Raticate", 20),
-    Pokemon("Spearow", 21),
-    Pokemon("Fearow", 22),
-    Pokemon("Ekans", 23),
-    Pokemon("Arbok", 24),
-    Pokemon("Pikachu", 25),
-    Pokemon("Raichu", 26),
-    Pokemon("Sandshrew", 27),
-    Pokemon("Sandslash", 28),
-    Pokemon("Nidoran♀", 29),
-    Pokemon("Nidorina", 30),
-    Pokemon("Nidoqueen", 31),
-    Pokemon("Nidoran♂", 32),
-    Pokemon("Nidorino", 33),
-    Pokemon("Nidoking", 34),
-    Pokemon("Clefairy", 35),
-)
-
-@Preview(showBackground = true)
-@Composable
-fun ListPreview() {
-    ListScreen()
-}
