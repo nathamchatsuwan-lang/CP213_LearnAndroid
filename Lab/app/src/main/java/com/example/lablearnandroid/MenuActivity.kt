900c08ae85701a0ab6c8be965c00440a3598805f
OneMoreHope <natham.chatsuwan@g.swu.ac.th>
2026-02-17 15:31:26 +0700

    commit

--- Lab/app/src/main/java/com/example/lablearnandroid/MenuActivity.kt
+++ Lab/app/src/main/java/com/example/lablearnandroid/MenuActivity.kt
@@ -34,6 +34,11 @@ class MenuActivity : ComponentActivity() {
                 }) {
                     Text("LifeCycleComposeActivity")
                 }
+                Button(onClick = {
+                    startActivity(Intent(this@MenuActivity, SharedPreferencesActivity::class.java))
+                }) {
+                    Text("LifeCycleComposeActivity")
+                }
             }
         }
     }
